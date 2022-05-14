cc59ebd80566f2369405350f3edd5de599f24ae6
smartphonecomputerundmehr <90951658+smartphonecomputerundmehr@users.noreply.github.com>
2022-05-14 16:27:26 +0200

    Add files via upload

---
+++ Blog/Blog_files/Blog.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20220514
+
+setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function photocastSubscribe()
+{photocastHelper("file://localhost/Users/user/Desktop/Neuer%20Ordner/SmartphoneComputerundmehr/Blog/rss.xml");}
+function onPageLoad()
+{loadMozillaCSS('Blog_files/BlogMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('../Media/transparent.gif');fixupAllIEPNGBGs();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ Blog/Eintrage/2022/5/14_Mac_mini_-_Late_2014_-_SSD_einbauen_files/14_Mac_mini_-_Late_2014_-_SSD_einbauen.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20220514
+
+setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('14_Mac_mini_-_Late_2014_-_SSD_einbauen_files/14_Mac_mini_-_Late_2014_-_SSD_einbauenMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
